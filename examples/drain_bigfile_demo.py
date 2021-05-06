9262c1ba14aa118b13d26d26ff4d5086f601a26f
David Ohana <david.ohana@ibm.com>
2021-05-06 14:53:08 +0300

    big file demo improvements

--- examples/drain_bigfile_demo.py
+++ examples/drain_bigfile_demo.py
@@ -31,33 +31,39 @@ if not os.path.isfile(in_log_file):
 
 config = TemplateMinerConfig()
 config.load(dirname(__file__) + "/drain3.ini")
+config.profiling_enabled = False
 template_miner = TemplateMiner(config=config)
 
 line_count = 0
+
+with open(in_log_file) as f:
+    lines = f.readlines()
+
 start_time = time.time()
 batch_start_time = start_time
 batch_size = 10000
-with open(in_log_file) as f:
-    for line in f:
-        line = line.rstrip()
-        line = line.partition(": ")[2]
-        result = template_miner.add_log_message(line)
-        line_count += 1
-        if line_count % batch_size == 0:
-            time_took = time.time() - batch_start_time
-            rate = batch_size / time_took
-            logger.info(f"Processing line: {line_count}, rate {rate:.1f} lines/sec, "
-                        f"{len(template_miner.drain.clusters)} clusters so far.")
-            batch_start_time = time.time()
-        if result["change_type"] != "none":
-            result_json = json.dumps(result)
-            logger.info(f"Input ({line_count}): " + line)
-            logger.info("Result: " + result_json)
+
+for line in lines:
+    line = line.rstrip()
+    line = line.partition(": ")[2]
+    result = template_miner.add_log_message(line)
+    line_count += 1
+    if line_count % batch_size == 0:
+        time_took = time.time() - batch_start_time
+        rate = batch_size / time_took
+        logger.info(f"Processing line: {line_count}, rate {rate:.1f} lines/sec, "
+                    f"{len(template_miner.drain.clusters)} clusters so far.")
+        batch_start_time = time.time()
+    if result["change_type"] != "none":
+        result_json = json.dumps(result)
+        logger.info(f"Input ({line_count}): " + line)
+        logger.info("Result: " + result_json)
 
 time_took = time.time() - start_time
 rate = line_count / time_took
-logger.info(f"--- Done processing file. Total of {line_count} lines, rate {rate:.1f} lines/sec, "
+logger.info(f"--- Done processing file in {time_took:.2f} sec. Total of {line_count} lines, rate {rate:.1f} lines/sec, "
             f"{len(template_miner.drain.clusters)} clusters")
+
 sorted_clusters = sorted(template_miner.drain.clusters, key=lambda it: it.size, reverse=True)
 for cluster in sorted_clusters:
     logger.info(cluster)
